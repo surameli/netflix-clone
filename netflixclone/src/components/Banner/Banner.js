e32087239ae648a35b31772bfd753df9c039976c
surameli <167941245+surameli@users.noreply.github.com>
2024-08-18 22:51:45 +0300

    banner

---
+++ netflixclone/src/components/Banner/Banner.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../../utils/axios';
+import "./banner.css";
+import requests from '../../utils/request';
+const Banner = () => {
+    const [movie, setmovie] = useState({});
+     useEffect(() => {
+        (async() =>{
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals)
+                setmovie(request.data.results[
+                    Math.floor(Math.random() * request.data.results.length)
+                ]);
+            } catch (error) {
+               console.log("error", error);
+                }
+        })()
+     },[]);
+   function truncate(str, n) {
+     return str?.length > n? str.substr(0, n - 1) + '...' :str;
+   }
+  return (
+    <div className='banner'
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }} >
+       <div className='banner-contain'>
+         <h1 className='banner-title'>
+             {movie?.title || movie?.name || movie?.original_name}
+         </h1>
+         <div className='banner-button'>
+            <button className='banner-button play'>play</button>
+            <button className='banner-button'>my lists</button>
+         </div>
+         <h1 className='banner-discription'>{truncate(movie?.overview, 150)}</h1>
+       </div>
+      <div className='banner-fade'/>
+    </div>
+  )
+}
+
+export default Banner
--- netflixclone/src/utils/request.js
+++ netflixclone/src/utils/request.js
@@ -1,19 +1,19 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const requests ={
- fetchTreanding:'/trending/all/day?api_key=${API_KEY}&language=en-US',
- fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=213',
- fetchTopRatedmovies:'/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1',
- fetchActionmovies:'discover/movie?api_key=${API_KEY}&with_genres=28',
- fetchAdventuremovies:'discover/movie?api_key=${API_KEY}&with_genres=12',
- fetchAnimationmovies:'discover/movie?api_key=${API_KEY}&with_genres=16',
- fetchComedymovies:'discover/movie?api_key=${API_KEY}&with_genres=35',
- fetchCrimemovies:'discover/movie?api_key=${API_KEY}&with_genres=80',
- fetchHorrormovies:'discover/movie?api_key=${API_KEY}&with_genres=27',
- fetchFantasymovies:'discover/movie?api_key=${API_KEY}&with_genres=14',
- fetchFamilymovies:'discover/movie?api_key=${API_KEY}&with_genres=10751',
- fetchDramamovies:'discover/movie?api_key=${API_KEY}&with_genres=18',
- fetchHistorymovies:'discover/movie?api_key=${API_KEY}&with_genres=36',
- fetchActionmovies:'discover/movie?api_key=${API_KEY}&with_genres=28',
- fetchTvShow: 'tv/popular?api_key=${API_KEY}&language=en-US&page=1'
+ fetchTreanding:`/trending/all/day?api_key=${API_KEY}&language=en-US`,
+ fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+ fetchTopRatedmovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+ fetchActionmovies:`discover/movie?api_key=${API_KEY}&with_genres=28`,
+ fetchAdventuremovies:`discover/movie?api_key=${API_KEY}&with_genres=12`,
+ fetchAnimationmovies:`discover/movie?api_key=${API_KEY}&with_genres=16`,
+ fetchComedymovies:`discover/movie?api_key=${API_KEY}&with_genres=35`,
+ fetchCrimemovies:`discover/movie?api_key=${API_KEY}&with_genres=80`,
+ fetchHorrormovies:`discover/movie?api_key=${API_KEY}&with_genres=27`,
+ fetchFantasymovies:`discover/movie?api_key=${API_KEY}&with_genres=14`,
+ fetchFamilymovies:`discover/movie?api_key=${API_KEY}&with_genres=10751`,
+ fetchDramamovies:`discover/movie?api_key=${API_KEY}&with_genres=18`,
+ fetchHistorymovies:`discover/movie?api_key=${API_KEY}&with_genres=36`,
+ fetchScienceFictionmovies:`discover/movie?api_key=${API_KEY}&with_genres=878`,
+ fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
 };
 export default requests;
